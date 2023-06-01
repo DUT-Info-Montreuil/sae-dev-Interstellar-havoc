7bf7b195e14be2174ac28d3e139313a3671307e1
elsa hadjadj <ehadjadj@iut.univ-paris8.fr>
2023-06-01 10:53:37 +0200

    Commentaire de la classe Cell

--- src/main/java/com/application/S2_dev/modele/bfs/Cell.java
+++ src/main/java/com/application/S2_dev/modele/bfs/Cell.java
@@ -1,28 +1,32 @@
 package com.application.S2_dev.modele.bfs;
 
+// Classe représentant une cellule dans la matrice
 public class Cell {
+    int x; 			// Coordonnée x de la cellule
+    int y; 			// Coordonnée y de la cellule
+    int dist;  		// Distance de la cellule par rapport au point de départ
+    Cell prev;  	// Cellule parente dans le chemin
 
-        int x;
-        int y;
-        int dist;  	//distance
-        Cell prev;  //parent cell in the path
+    // Constructeur de la classe Cell
+    Cell(int x, int y, int dist, Cell prev) {
+        this.x = x;
+        this.y = y;
+        this.dist = dist;
+        this.prev = prev;
+    }
 
-        Cell(int x, int y, int dist, Cell prev) {
-            this.x = x;
-            this.y = y;
-            this.dist = dist;
-            this.prev = prev;
-        }
+    // Méthode toString pour afficher les coordonnées de la cellule
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
 
-        @Override
-        public String toString() {
-            return "(" + x + "," + y + ")";
-        }
-
+    // Getter pour obtenir la coordonnée x de la cellule
     public int getX() {
         return x;
     }
 
+    // Getter pour obtenir la coordonnée y de la cellule
     public int getY() {
         return y;
     }
