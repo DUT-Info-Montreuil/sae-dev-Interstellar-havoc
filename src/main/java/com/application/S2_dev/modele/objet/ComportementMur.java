5f0b310272dd3dd3c0b8286f35366fb39ac6a86e
ISAF Mehdi <m.isaf@iut.univ-paris8.fr>
2023-10-18 16:22:25 +0200

    Version qui marche avec les ennemis

--- src/main/java/com/application/S2_dev/modele/objet/ComportementMur.java
+++ src/main/java/com/application/S2_dev/modele/objet/ComportementMur.java
@@ -3,7 +3,7 @@ package com.application.S2_dev.modele.objet;
 public class ComportementMur implements Comportement {
     @Override
     public void agit(Objet objet) {
-        // Le mur n'agit pas
+
     }
     @Override
     public void degats(int value, Objet objet) {
